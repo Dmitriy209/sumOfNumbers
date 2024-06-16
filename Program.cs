9b56d3e15a69108dca50abfb64f7e18b937e998a
Dmitriy209 <dim286754@gmail.com>
2024-06-16 15:08:12 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(0, 100);
+            int sum = 0;
+
+            Console.WriteLine($"Случайное число {number}.");
+
+            for (int i = 1; i <= number; i++)
+            {
+                int remainderThree = i % 3;
+                int remainderFive = i % 5;
+                bool remainder = remainderThree == 0 || remainderFive == 0;
+
+                if (remainder == true)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine($"Сумма всех положительных чисел меньше {number} (включая число), которое кратно 3 или 5 равно {sum}");
+        }
+    }
+}
